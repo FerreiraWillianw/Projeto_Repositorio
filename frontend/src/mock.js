08abcc39be17badce285e716fa02d9c1dd54a737
FerreiraWillianw <willian.ferreiraw30@gmail.com>
2026-05-17 17:54:58 -0300

    criação de uma nova tela

--- frontend/src/mock.js
+++ frontend/src/mock.js
@@ -44,4 +44,14 @@ export const projectsData = [
     powerbiUrl: "https://vendas-amazon-india.streamlit.app/?embed=true", // Coloque o link público do seu app Streamlit aqui
     highlights: ["Quantidade de produtos por categoria", "Preço Original vs. Preço com Desconto", "Relação Preço vs. Nota (Rating)", "Top 10 Produtos Mais Populares"]
   }
+
+  {
+    id: 3,
+    title: "Em Desenvolvimento..",
+    description: "Em Desenvolvimento..",
+    technologies: ["Em Desenvolvimento.."],
+    image: FotoDashAmazon, // Se você tiver um print do dashboard escuro, pode importar e colocar aqui depois!
+    powerbiUrl: "Em Desenvolvimento..", // Coloque o link público do seu app Streamlit aqui
+    highlights: ["Em Desenvolvimento.."]
+  }
 ];

3a900324335f9cc6011896cb75adbd0900f7a302
FerreiraWillianw <willian.ferreiraw30@gmail.com>
2026-05-17 17:39:06 -0300

    adição de um novo quadro de dashboard

--- frontend/src/mock.js
+++ frontend/src/mock.js
@@ -36,11 +36,11 @@ export const projectsData = [
 
   {
     id: 2,
-    title: "Em desenvolvimento",
-    description: "Relatório em desenvolvimento",
-    technologies: ["Em desenvolvimento"],
-    image: "",
-    powerbiUrl: "https://app.powerbi.com/view?r=eyJrIjoiOWYxNjFlMzUtMDA2OC00ZWIyLTlhZGEtOTNkZGE1MjE1YmMwIiwidCI6IjJlZmMxNDk2LTE4NTktNDEyNy1iMDc4LTUwMTBhNGZlNGI5asx9",
-    highlights: ["Em desenvolvimento"]
+    title: "Análise de Vendas e Mercado - Amazon",
+    description: "Dashboard interativo desenvolvido em Python e Streamlit para monitorar o catálogo de produtos da Amazon India, fonte obitda do Kaggle. O relatório cruza dados de preços originais, valores com desconto, popularidade de produtos e satisfação do cliente para identificar os campeões de vendas e os setores com os descontos mais agressivos.",
+    technologies: ["Python", "Pandas", "Streamlit", "Plotly"],
+    image: "", // Se você tiver um print do dashboard escuro, pode importar e colocar aqui depois!
+    powerbiUrl: "https://vendas-amazon-india.streamlit.app/", // Coloque o link público do seu app Streamlit aqui
+    highlights: ["Quantidade de produtos por categoria", "Preço Original vs. Preço com Desconto", "Relação Preço vs. Nota (Rating)", "Top 10 Produtos Mais Populares"]
   }
 ];
